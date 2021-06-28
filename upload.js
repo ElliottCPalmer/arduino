fb1e79d908d9d3bd9d1615fa2c75260ed89dfb63
ElliottCPalmer <elliott.palmer@outlook.com>
2021-06-28 15:15:14 +0100

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,9 +1,9 @@
-var storageRef = firebase.storageRef();
-var mountainsRef = storageRef.child('mountains.jpg');
-var mountainImageRef = storageRef.child('images/mountains.jpg');
-mountainsRef.name === mountainImageRef.name;
-mountainsRef.fullPath === mountainImageRef.fullPath;
+// var storageRef = firebase.storageRef();
+// var mountainsRef = storageRef.child('mountains.png');
+// var mountainImageRef = storageRef.child('images/mountains.png');
+// mountainsRef.name === mountainImageRef.name;
+// mountainsRef.fullPath === mountainImageRef.fullPath;
 
-ref.put(File).then((snapshot) => {
+ref.put(Users/Ellio/Documents/mountains.png).then((snapshot) => {
     console.log('uploaded a file');
 });
