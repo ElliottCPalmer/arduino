b618cc27e725cfca87648d7dd1c529362e3aaa1a
ElliottCPalmer <elliott.palmer@outlook.com>
2021-06-29 10:20:12 +0100

    Update testFirestore.js

--- testFirestore.js
+++ testFirestore.js
@@ -1,10 +1,11 @@
+  var docRef = "powerstate";
   var db = firebase.firestore();
   db.collection("users").add({
     first: "AFa",
     last: "Lovelace",
     born: 1815
   })
-  var docRef = powerstate
+
   .then((docRef) => {
     console.log("Document written with ID: ", docRef.id);
   })
